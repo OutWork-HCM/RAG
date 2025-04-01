7dd953e76940fb93c389de7c4f8054567c5fe7fa
Huynh Van Vinh <vinhhv@live.com>
2025-04-01 09:00:09 +0700

    Updated setup_vps.sh

--- setup_vps.sh
+++ setup_vps.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Cấu hình VPS
-VPS_IP="your_vps_ip"
+VPS_IP="myvps"
 VPS_USER="root"
 WORK_DIR="/work"
 ANACONDA_INSTALLER="Anaconda3-2024.10-1-Linux-x86_64.sh"
 ANACONDA_URL="https://repo.anaconda.com/archive/$ANACONDA_INSTALLER"
+ANACONDA_DIR="/opt/anaconda3"
 
 # Load mật khẩu từ file .env
 export $(grep VPS_ROOT .env | xargs)
@@ -21,18 +22,18 @@ EOF
 # Sao chép các file cần thiết lên VPS
 sshpass -p "$VPS_ROOT" scp *.py $VPS_USER@$VPS_IP:$WORK_DIR
 sshpass -p "$VPS_ROOT" scp environment.yml $VPS_USER@$VPS_IP:$WORK_DIR
+sshpass -p "$VPS_ROOT" scp .env $VPS_USER@$VPS_IP:$WORK_DIR
 
 # Cài đặt Anaconda và thiết lập môi trường
 sshpass -p "$VPS_ROOT" ssh $VPS_USER@$VPS_IP << EOF
 cd $WORK_DIR
 wget $ANACONDA_URL
-bash $ANACONDA_INSTALLER -b -p /root/anaconda3
-echo 'export PATH="/root/anaconda3/bin:\$PATH"' >> ~/.bashrc
-source ~/.bashrc
-conda deactivate
-conda env create -f environment.yml
-conda activate RAG_Marker
-streamlit run app.py
+bash $ANACONDA_INSTALLER -b -p $ANACONDA_DIR
+$ANACONDA_DIR/bin/conda init
+EOF
+sshpass -p "$VPS_ROOT" ssh $VPS_USER@$VPS_IP << EOF
+cd $WORK_DIR
+$ANACONDA_DIR/bin/conda env create -f environment.yml
 EOF
 
 echo "Setup VPS hoàn tất!"
