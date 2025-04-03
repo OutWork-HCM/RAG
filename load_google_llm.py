0b93c807b6af602eb4bd9ac73553c4e21bcc46ab
Huynh Van Vinh <vinhhv@live.com>
2025-04-03 13:43:05 +0700

    Removed unnecessary files

--- load_google_llm.py
+++
@@ -1,5 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
-model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it")
